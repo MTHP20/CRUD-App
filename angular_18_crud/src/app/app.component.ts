442d39e385ba4e187507ca3b6a9c3355d0e72178
Michael <mtpanlilio20@hotmail.com>
2024-11-19 21:29:44 +0000

    Initalised Employee Object

--- angular_18_crud/src/app/app.component.ts
+++ angular_18_crud/src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_18_crud';
+
+  employeeForm?: FormGroup;
+
+  employeeObj: EmployeeModel = new EmployeeModel();
+
+  constructor() {
+    this.createForm()
+  }
+
+  createForm() {
+    this.employeeForm = new FormGroup({
+      empid: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      address: new FormControl(this.employeeObj.address),
+      contactNo: new FormControl(this.employeeObj.contactNo),
+      emailID: new FormControl(this.employeeObj.emailID),
+      pinCode: new FormControl(this.employeeObj.pinCode),
+      state: new FormControl(this.employeeObj.state),
+    })
+  }
 }
